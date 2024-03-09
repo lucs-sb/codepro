c2efeb84bf1e167b1417ac48657b41ff3d223a89
Lucas Soares <soares0lucas8@gmail.com>
2024-03-09 15:03:05 -0300

    Estrutura inicial

---
+++ src/main/java/br/com/lucassoaresdev/codepro/CodeProCursosApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucassoaresdev.codepro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeProCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeProCursosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lucassoaresdev/codepro/CodeProCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucassoaresdev.codepro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeProCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
